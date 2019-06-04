110b65099e9b87050e39053fd803ff43e2637aa7
Beckford <abeckford@tre.state.ma.us>
2019-06-04 13:12:23 -0400

    final post for vacations r us exercise

--- in-class-hotel.js
+++ in-class-hotel.js
@@ -5,32 +5,12 @@
 // -has a gym - NO
 // -has a pool - YES
 // -allows pets - NO
-
-let hotel1 = {
-	name: "hotel Paradise",
-	rooms: 50,
-	hasGym: false,
-	hasPool: true,
-	allowsPets: false,
-	booked: 0,};
-
-
 // 2. Create a second variable for Quay Hotel
 // -has 75 total rooms
 // -keeps track of the number of booked rooms - set to 0
 // -has a gym - YES
 // -has a pool - YES
 // -allows pets - YES
-
-let hotel2 = {
-	name: "Quay Hotel",
-	rooms: 75,
-	hasGym: true,
-	hasPool: true,
-	allowsPets: true,
-	booked: 0,};
-
-
 // 3. Create a third variable for Parker Place
 // -has 140 total rooms
 // -keeps track of the number of booked rooms - set to 0
@@ -39,43 +19,72 @@ let hotel2 = {
 // -allows pets - YES
 
 
-	
+let hotels = [{
+	name: "Hotel Paradise",
+	totalRms: 50,
+	bookedRms: 0,
+	gym: 'no',
+	pool: 'yes',
+	pets: 'no',
+	numOfRoomsAvailable: function () {
+		return this.totalRms - this.bookedRms;
+	},
+	roomTypes: ['single', 'double', 'triple', 'king', 'suite'],
+}, {
+	name: "Quay Hotel",
+	totalRms: 75,
+	bookedRms: 0,
+	gym: 'yes',
+	pool: 'yes',
+	pets: 'yes',
+	numOfRoomsAvailable: function () {
+		return this.totalRms - this.bookedRms;
+	},
+	roomTypes: ['single', 'double', 'triple'],
+}, {
+	name: "Parker Place",
+	totalRms: 140,
+	bookedRms: 0,
+	gym: 'no',
+	pool: 'yes',
+	pets: 'no',
+	numOfRoomsAvailable: function () {
+		return this.totalRms - this.bookedRms;
+	},
+	roomTypes: ['single', 'double', 'king', 'suite'],
+}]
 
-	let hotel3 = {
-		name: "Parker Place",
-		rooms: 140,
-		hasGym: true,
-		hasPool: true,
-		allowsPets: false,
-		booked: 0,};
-	
-	
 
 
 // 4. Print the name of each hotel on a separate line
+console.log("-------------\n");
+console.log("4. Hotel Names");
 
 
-  let buildings = ["Hotel Paradise", "Quay Hotel", "Parker Place"];
-for(let i=0; i < buildings.length; i++){
-	console.log("This hotel is called " + buildings[i].name);
+for (let i = 0; i < hotels.length; i++) {
+	console.log(hotels[i].name);
 }
-   
-
 // 5. Use square bracket syntax to print the total number of rooms each hotel has
 
 
-let rooms = [50, 75, 140];
+console.log("-------------\n");
+console.log("5. Total Rooms");
 
-for(let i = 0; i < rooms.length; i++) {
-	console.log("    " + rooms[i].name);
-
+for (let i = 0; i < hotels.length; i++) {
+	console.log(hotels[i].totalRms);
 }
 
-console.log("\n");
+
 
 // 6. Since the hotel has yet to accept bookings, confirm none of the rooms are booked by printing the
 // number of rooms booked
 
+console.log("-------------\n");
+console.log("6. Rooms Booked");
+
+for (let i = 0; i < hotels.length; i++) {
+	console.log(hotels[i].bookedRms);
+}
 
 
 // 7. Print the names of hotels that have gyms
@@ -83,12 +92,53 @@ console.log("\n");
 //console.log("7. Hotels with Gyms");
 
 
+console.log("-------------\n");
+console.log("7. Hotels with Gyms");
+for (let i = 0; i < hotels.length; i++) {
+	if (hotels[i].gym === 'yes') {
+		console.log(hotels[i].name + ' has a gym');
+	}
+}
+
+
+
 // 8. Calculate and print the number of rooms available per hotel
 //console.log("-------------\n");
 //console.log("8. Rooms Available");
+
+
+console.log("-------------\n");
+console.log("8. Rooms Available");
+
+
+
+let availableRooms = 0;
+for (let i = 0; i < hotels.length; i++) {
+	availableRooms = hotels[i].totalRms - hotels[i].bookedRms;
+	console.log('There are ' + availableRooms + ' rooms available at ' + hotels[i].name);
+}
+
 
 
 // 9. The Marketing Team believes the new site has 500 rooms, but the Construction Team says less than 300.
 // To resolve this squabble, print the total number of rooms by adding this across all 3 hotels.
 //console.log("-------------\n");
 //console.log("9. Total Rooms Across All Hotels");
+
+
+console.log("-------------\n");
+console.log("9. Total Rooms Across All Hotels");let roomArray;
+
+
+
+let sumOfAllRooms = 0;
+for (let i = 0; i < hotels.length; i++) {
+	roomArray = hotels[i].totalRms;
+	sumOfAllRooms += roomArray;
+}
+
+
+
+
+console.log('9. The total number of rooms is ' + sumOfAllRooms);
+
